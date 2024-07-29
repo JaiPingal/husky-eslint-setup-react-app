63a44489d1f7675ecadccf30b4c1e48db98b84cd
JaiPrakashPingalExternLabs <jaiprakashpingal@externlabs.com>
2024-07-29 15:42:38 +0530

    first commit

---
+++ src/SignupButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SignupButton = () => {
+  const handleSignup = () => {
+    alert('Sign up successful!');
+  };
+
+  return (
+    <button onClick={handleSignup} className='signup-button'>
+      Sign Up
+    </button>
+  );
+};
+export default SignupButton;
