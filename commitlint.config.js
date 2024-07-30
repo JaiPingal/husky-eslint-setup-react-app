ad74304a7496225f843d4fb5dc7b273fbf0e63d9
JaiPrakashPingalExternLabs <jaiprakashpingal@externlabs.com>
2024-07-30 11:38:29 +0530

    feat: husky setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,30 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [2, "always", ["feat", "fix", "chore", "docs", "style", "refactor", "test", "ci", "add"]],
-      },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'ci',
+        'add'
+      ]
+    ],
+    'type-empty': [
+      2, 
+      'never', 
+      'Commit message type is required. Please specify the type (e.g., feat, fix, chore, etc.)'
+    ],
+    'subject-empty': [
+      2, 
+      'never', 
+      'Commit message subject is required. Please provide a brief description of the change.'
+    ],
+  },
 };
